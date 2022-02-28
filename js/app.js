83f80c470af67d9dd1ebbd64e5355edaa0c712f0
Prokash Pul <pprokash102@gmail.com>
2022-02-28 21:53:21 +0600

    search button and input filel add event handel

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
 // load api 
-const mobileApiLoad = () => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=`
+const mobileApiLoad = (inputData) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputData}`
     // fetch function called
     fetch(url)
-        .then(res => {
+        .then((res) => {
+            // condition apply
             if (res.status >= 200 && res.status <= 299) {
                 return res.json();
             } else {
                 throw Error(res.statusText);
             }
         })
-        .then(mobileData => console.log(mobileData));
+        .then((mobileData) => {
+            mobileDataDisplay(mobileData)
+        })
+        .catch((error) => {
+            //error handel
+            console.log(error)
+        })
 }
-mobileApiLoad()
+
+// search button and input field  event handel add 
+const searchBtn = () => {
+    const inputField = document.getElementById('search-input');
+    const inputValue = inputField.value;
+    mobileApiLoad(inputValue.toLowerCase());
+    // input value empty
+    inputField.value = '';
+}
+
+// Mobile api data load in display
+const mobileDataDisplay = (data) => {
+    console.log(data)
+}
