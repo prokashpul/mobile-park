93951332e6a25eb3f4ef32ec6737e560423cf45c
Prokash Pul <pprokash102@gmail.com>
2022-02-28 23:36:12 +0600

    dispaly show items and error handel

--- js/app.js
+++ js/app.js
@@ -8,19 +8,23 @@ const mobileApiLoad = (inputData) => {
             if (res.status >= 200 && res.status <= 299) {
                 return res.json();
             } else {
-                throw Error(res.statusText);
+
             }
         })
         .then((mobileData) => {
             mobileDataDisplay(mobileData)
-        }).catch(error => console.log(error))
+        }).catch(error => document.getElementById('items-number').innerText = `OOps!! 😭😭 ${error.statusText}`)
 }
 
 // search button and input field  event handel add 
 const searchBtn = () => {
     const inputField = document.getElementById('search-input');
     const inputValue = inputField.value;
-    mobileApiLoad(inputValue.toLowerCase());
+    if (inputValue == "") {
+        document.getElementById('items-number').innerText = `OOps!! 😭😭 Please input any phone name...`;
+    } else {
+        mobileApiLoad(inputValue.toLowerCase());
+    }
     // input value empty
     inputField.value = '';
 }
@@ -28,18 +32,26 @@ const searchBtn = () => {
 // Mobile api data load in display
 const mobileDataDisplay = (data) => {
     const displayItems = document.getElementById('display-items');
+    // empty display Items
     displayItems.textContent = '';
-    data.data.slice(0, 20).forEach((mobile, index) => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-
+    // use forEach loop get search items
+    if (data.data.length <= 20) {
+        document.getElementById('items-number').innerText = `Search Results  ( ${data.data.length}  items Found)`;
+    } else {
+        document.getElementById('items-number').innerText = `Search Results  (20 item Found)`;
+    }
+    data.data.slice(0, 20).forEach((mobile) => {
+        const div = document.createElement('div'); //create a div element
+        div.classList.add('col'); //add a class 
+        console.log(mobile)
+        // div inner html setup
         div.innerHTML = `
-        <div class="card h-100">
-             <img src="img/mobile.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.</p>
+        <div class="card border-0 shadow rounded h-100">
+             <img src="${mobile.image?mobile.image:'img/mobile.jpg'}" class="card-img-top px-4 pt-4" height="380" alt="${mobile.phone_name?mobile.phone_name:'Not Found'}">
+                <div class="card-body text-center my-3">
+                  <h3 class="card-title">${mobile.phone_name?mobile.phone_name:'Not Found'}</h3>
+                    <p>Brand: <em> ${mobile.brand?mobile.brand:'Not Found'}</em></p>
+                    <a class="btn btn-warning rounded-0 text-light">About More </a>
              </div>
             </div>
         `
