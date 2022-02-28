c3514aa33df0ac1dab891a10b190e6a72d8ac141
Prokash Pul <pprokash102@gmail.com>
2022-02-28 22:44:59 +0600

    show search result in dispaly

--- js/app.js
+++ js/app.js
@@ -13,11 +13,7 @@ const mobileApiLoad = (inputData) => {
         })
         .then((mobileData) => {
             mobileDataDisplay(mobileData)
-        })
-        .catch((error) => {
-            //error handel
-            console.log(error)
-        })
+        }).catch(error => console.log(error))
 }
 
 // search button and input field  event handel add 
@@ -31,5 +27,22 @@ const searchBtn = () => {
 
 // Mobile api data load in display
 const mobileDataDisplay = (data) => {
-    console.log(data)
+    const displayItems = document.getElementById('display-items');
+    displayItems.textContent = '';
+    data.data.slice(0, 20).forEach((mobile, index) => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+
+        div.innerHTML = `
+        <div class="card h-100">
+             <img src="img/mobile.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.</p>
+             </div>
+            </div>
+        `
+        displayItems.appendChild(div)
+    })
 }
